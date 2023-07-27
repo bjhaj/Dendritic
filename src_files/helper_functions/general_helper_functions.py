4ed51dda457a20f357f6c0034276a9bd1be26bcd
Baaz Jhaj <64169433+bjhaj@users.noreply.github.com>
2023-07-27 13:51:49 -0700

    Update general_helper_functions.py

--- src_files/helper_functions/general_helper_functions.py
+++ src_files/helper_functions/general_helper_functions.py
@@ -32,15 +32,23 @@ class AverageMeter:
         self.avg = self.sum / self.count
 
 
-def accuracy(output, target, topk=(1,)):
-    """Computes the accuracy over the k top predictions for the specified values of k"""
-    maxk = max(topk)
-    batch_size = target.size(0)
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target.reshape(1, -1).expand_as(pred))
-    return [correct[:k].reshape(-1).float().sum(0) * 100. / batch_size for k in topk]
+#def accuracy(output, target, topk=(1,)):
+  #  """Computes the accuracy over the k top predictions for the specified values of k"""
+  #  maxk = max(topk)
+  #  batch_size = target.size(0)
+  #  _, pred = output.topk(maxk, 1, True, True)
+   # pred = pred.t()
+   # correct = pred.eq(target.reshape(1, -1).expand_as(pred))
+   # return [correct[:k].reshape(-1).float().sum(0) * 100. / batch_size for k in topk]
 
+def accuracy(output, target):
+    """Computes the accuracy for the top-1 prediction"""
+    batch_size = target.size(0)
+    _, pred = output.topk(1, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+    top1_accuracy = correct[:1].view(-1).float().sum(0) * 100. / batch_size
+    return top1_accuracy
 
 def silence_PIL_warnings():
     import PIL
