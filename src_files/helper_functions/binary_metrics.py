41dd87f14b4df7cebeb9f060207b0008c48fe452
Baaz Jhaj <64169433+bjhaj@users.noreply.github.com>
2023-07-27 23:13:14 -0700

    Update binary_metrics.py

--- src_files/helper_functions/binary_metrics.py
+++ src_files/helper_functions/binary_metrics.py
@@ -8,50 +8,43 @@ from src_files.helper_functions.general_helper_functions import accuracy, Averag
 from src_files.helper_functions.distributed import print_at_master, to_ddp, reduce_tensor, num_distrib, setup_distrib
 from torch.cuda.amp import GradScaler, autocast
 
-def binaryROC(args, model, dataset):
-    # Set the device for computation (CPU or GPU)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    # Put the model in evaluation mode
+def binaryROC(model, dataloader):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    FIXED_FPR = 0.05
     model.eval()
-
-    # Create an empty array to store the predicted probabilities for the positive class (class 1)
-    y_pred_proba = np.zeros((len(dataset),))
-
-    # Iterate over the test dataset and generate predictions
+    y_true = []
+    y_pred = []
     with torch.no_grad():
-        for i, (inputs, _) in enumerate(dataset):
+        for inputs, labels in dataloader:
             inputs = inputs.to(device)
-            outputs = model(inputs.unsqueeze(0))
-            probabilities = torch.sigmoid(outputs).squeeze(0)  # Use sigmoid activation for binary classification
-            y_pred_proba[i] = probabilities.item()
+            labels = labels.to(device)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            y_true.extend(labels.cpu().numpy())
+            y_pred.extend(preds.cpu().numpy())
 
-    # Convert true labels to binary format (0 or 1)
-    y_true = np.array(dataset.targets)
+    y_true = torch.tensor(y_true)  # Convert y_true to a PyTorch tensor
+    y_pred = torch.tensor(y_pred)  # Convert y_pred to a PyTorch tensor
 
-    # Create a color palette for the line
-    color_palette = plt.cm.get_cmap('tab10')
+    fprs, tprs, thresholds = roc_curve(y_true, y_pred)
 
-    # Plot ROC curve for the positive class (class 1)
+    # Plot the ROC curve
     plt.figure(figsize=(6, 6))
-    y_true_class = y_true
-    y_pred_class = y_pred_proba  # Positive class probabilities for binary classification
-
-    fpr, tpr, thresholds = roc_curve(y_true_class, y_pred_class)
-    auc = roc_auc_score(y_true_class, y_pred_class)
-
-    # Plot the ROC curve for the positive class
-    plt.plot(fpr, tpr, color=color_palette(0), label=f'Dendrite 1, AUC = {auc:.2f}')
-
-    # Plot a line representing random chance (AUC = 0.5)
+    plt.plot(fprs, tprs, label='ROC curve')
     plt.plot([0, 1], [0, 1], color='gray', linestyle='--', label='Random (AUC = 0.5)')
-
-    # Set labels and title for the plot
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title('ROC Curve - Binary Classification')
+    plt.title('ROC Curve')
     plt.legend()
     plt.show()
+
+    tpr = tprs[fprs < FIXED_FPR][-1]
+    fpr = fprs[fprs < FIXED_FPR][-1]
+    threshold = thresholds[fprs < FIXED_FPR][-1]
+
+    print("AUC:", roc_auc_score(y_true, y_pred))
+    to_pct = lambda x: str(round(x, 4) * 100) + "%"
+    print("TPR: ", to_pct(tpr), "\nFPR: ", to_pct(fpr), "\nThreshold: ", round(threshold, 2))
 
 
 def bin_calculate_precision_recall_f1(y_true, y_pred):
